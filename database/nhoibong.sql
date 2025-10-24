326e6c37272c443b70cc5e1129cae9dcce71d496
hathuu1824 <hathuu1824@users.noreply.github.com>
2025-10-24 20:44:37 +0700

    Add database SQL file

---
+++ database/nhoibong.sql
@@ -0,0 +1,153 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               9.2.0 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.10.0.7000
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for nhoibong
+CREATE DATABASE IF NOT EXISTS `nhoibong` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `nhoibong`;
+
+-- Dumping structure for table nhoibong.banners
+CREATE TABLE IF NOT EXISTS `banners` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url_anh` varchar(255) NOT NULL,
+  `an_hien` tinyint(1) DEFAULT '1',
+  `thu_tu` int DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table nhoibong.banners: ~4 rows (approximately)
+INSERT INTO `banners` (`id`, `url_anh`, `an_hien`, `thu_tu`) VALUES
+	(1, 'slide4.png', 1, 1),
+	(2, 'slide3.png', 1, 2),
+	(3, 'slide2.png', 1, 3),
+	(4, 'slide1.png', 1, 4);
+
+-- Dumping structure for table nhoibong.sanpham
+CREATE TABLE IF NOT EXISTS `sanpham` (
+  `masp` int NOT NULL AUTO_INCREMENT,
+  `tensp` varchar(255) NOT NULL,
+  `giatien` decimal(12,2) NOT NULL,
+  `mota` text,
+  `anhsp` varchar(255) DEFAULT NULL,
+  `noibat` tinyint NOT NULL DEFAULT '0',
+  `bst` varchar(50) DEFAULT NULL,
+  `loai` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`masp`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table nhoibong.sanpham: ~9 rows (approximately)
+INSERT INTO `sanpham` (`masp`, `tensp`, `giatien`, `mota`, `anhsp`, `noibat`, `bst`, `loai`) VALUES
+	(1, 'Gấu bông có kèm chăn', 90000.00, 'Chăn gối văn phòng 3 trong 1', 'c5c9e4e3dfeece6a7f9aaac418e1086e.jfif', 0, 'khong', 'changoi'),
+	(2, 'Thú nhồi bông Sanrio', 105000.00, 'Thú nhồi bông Sanrio (Kuromi, Cinnamoroll, MyMelody,...) lông mịn', '20230116_G5nEiVQo5U5UQaRo.jpg', 1, 'sanrio', 'tnb'),
+	(3, 'Móc khóa gấu trúc đỏ dễ thương', 65000.00, 'Móc khóa gấu trúc đỏ nhồi bông kéo được đuôi dễ thương', 'sg-11134201-7ren6-m1qdnfz1sgug03.jfif', 1, 'khong', 'mockhoa'),
+	(4, 'Thú nhồi bông Doraemon mặc trang phục Rock cỡ vừa', 110500.00, 'Thú nhồi bông Doraemon mặc trang phục Rock cỡ vừa', 'images.jfif', 0, 'doraemon', 'tnb'),
+	(5, 'Gấu bông trắng cỡ lớn kèm gấu cỡ nhỏ', 120000.00, 'Gấu bông trắng đeo nơ cổ cỡ lớn kèm gấu trắng cỡ nhỏ', 'gau-bong-dep-tn.jpg', 1, 'khong', 'tnb'),
+	(6, 'Thú nhồi bông Capybara gõ mõ', 85000.00, 'Thú nhồi bông Capybara gõ mõ ngộ nghĩnh - Size 35cm', '4384959201a0a8bfeec9dff8f1e6e8c6.jpeg', 0, 'capybara', 'tnb'),
+	(7, 'Dây buộc rèm gắn hình thú', 55000.00, 'Dây buộc rèm gắn hình thú nhồi bông đáng yêu', 'Day-buoc-rem-gan-hinh-thu-nhoi-bong-dang-yeu-FSH7454-14.jpg', 0, 'khong', 'khac'),
+	(8, 'Thú nhồi bông Baby Three', 195500.00, 'Thú nhồi bông Baby Three với các loại mắt khác nhau', 'san-pham-1-1-1738991026-3249-1-2791-1254-1739035001.jpg', 0, 'babythree', 'tnb'),
+	(9, 'Thú nhồi bông bạch tuộc 2 cảm xúc', 130000.00, 'Thú nhồi bông bạch tuộc 2 cảm xúc lông mịn 3D nhiều kích thước', 'unnamed.jpg', 0, 'khong', 'tnb');
+
+-- Dumping structure for table nhoibong.taikhoan
+CREATE TABLE IF NOT EXISTS `taikhoan` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tendangnhap` varchar(50) NOT NULL,
+  `matkhau` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `vaitro` enum('user','admin') NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tendangnhap` (`tendangnhap`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table nhoibong.taikhoan: ~5 rows (approximately)
+INSERT INTO `taikhoan` (`id`, `tendangnhap`, `matkhau`, `email`, `vaitro`) VALUES
+	(1, 'admin', '123456', 'admin@example.com', 'admin'),
+	(2, 'user', 'user123', 'user@example.com', 'user'),
+	(3, 'suabien', 'suabien', 'suabien@gmail.com', 'user'),
+	(4, 'acc', '1234567', 'acc@gmail.com', 'user'),
+	(5, 'acctest', '12345678', 'acctest@gmail.com', 'user');
+
+-- Dumping structure for table nhoibong.tt_user
+CREATE TABLE IF NOT EXISTS `tt_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `taikhoan_id` int DEFAULT NULL,
+  `hoten` varchar(120) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `ngaysinh` date DEFAULT NULL,
+  `sdt` varchar(20) DEFAULT NULL,
+  `diachi` varchar(255) DEFAULT NULL,
+  `anh` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_userprofile_account` (`taikhoan_id`),
+  CONSTRAINT `fk_userprofile_account` FOREIGN KEY (`taikhoan_id`) REFERENCES `taikhoan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table nhoibong.tt_user: ~0 rows (approximately)
+INSERT INTO `tt_user` (`id`, `taikhoan_id`, `hoten`, `email`, `ngaysinh`, `sdt`, `diachi`, `anh`) VALUES
+	(1, 1, 'Nguyễn Văn A', 'admin@example.com', '1990-04-15', '0901234567', '68 Nguyễn Chí Thanh, Đống Đa, Hà Nội', 'uploads/avatars/admin.jpg'),
+	(2, 2, 'Trần Thị B', 'user@example.com', '1995-08-21', '0912345678', '12 Lý Thường Kiệt, Hoàn Kiếm, Hà Nội', 'uploads/avatars/user.jpg'),
+	(3, 3, 'Phạm Hữu C', 'suabien@gmail.com', '1998-11-02', '0933456789', '22 Nguyễn Huệ, Quận 1, TP. Hồ Chí Minh', 'uploads/avatars/suabien.jpg'),
+	(4, 4, 'Lê Hoàng D', 'acc@gmail.com', '2000-01-10', '0944567890', '45 Điện Biên Phủ, Bình Thạnh, TP. Hồ Chí Minh', 'uploads/avatars/acc.jpg'),
+	(5, 5, 'Đỗ Minh E', 'acctest@gmail.com', '1999-06-25', '0965678901', '8 Trần Phú, Nha Trang, Khánh Hòa', 'uploads/avatars/acctest.jpg');
+
+-- Dumping structure for trigger nhoibong.trg_taikhoan_email_to_ttuser
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `trg_taikhoan_email_to_ttuser` AFTER UPDATE ON `taikhoan` FOR EACH ROW BEGIN
+  IF NEW.email IS NOT NULL AND NEW.email <> OLD.email THEN
+    UPDATE tt_user
+       SET email = NEW.email
+     WHERE taikhoan_id = NEW.id;
+  END IF;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dumping structure for trigger nhoibong.trg_ttnd_before_insert_email
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `trg_ttnd_before_insert_email` BEFORE INSERT ON `tt_user` FOR EACH ROW BEGIN
+  DECLARE v_email VARCHAR(150);
+  SELECT email INTO v_email
+  FROM taikhoan
+  WHERE id = NEW.taikhoan_id
+  LIMIT 1;
+
+  IF v_email IS NOT NULL AND v_email <> '' THEN
+    SET NEW.email = v_email;
+  END IF;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dumping structure for trigger nhoibong.trg_update_email_taikhoan_to_ttnd
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `trg_update_email_taikhoan_to_ttnd` AFTER UPDATE ON `taikhoan` FOR EACH ROW BEGIN
+  IF NEW.email IS NOT NULL AND NEW.email <> OLD.email THEN
+    UPDATE tt_user
+       SET email = NEW.email
+     WHERE taikhoan_id = NEW.id;
+  END IF;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
